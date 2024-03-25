99c317b6d7b44e112d5e117c9fea9c9d126023df
unknown <jmorofsky@gmail.com>
2024-03-24 22:35:03 -0400

    Update Summary.js

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,8 +4,8 @@ import '../css/Summary.css'
 function Summary(props) {
     return (
         <div className='summary' style={props.style}><strong>Born in 1999 in South Florida. Since I was a
-            child, I have loved computers and technology. I express myself through the web.
-            It is possible to create beauty and efficency using the internet.</strong>
+            child, I have loved computers and technology. Expert software QA engineer, ISC2 member, and
+            hobby web developer.</strong>
         </div>
     )
 }
